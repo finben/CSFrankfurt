0b5c090dec4dcf6bf6869d29a2c000fa71219ae8
finben <benfindeisen@gmail.com>
2022-04-02 23:14:25 +0200

    toggle-addition

--- lang.js
+++ lang.js
@@ -3,28 +3,42 @@ $(":lang(en)").hide();
 $(":lang(de)").hide();
 $(":lang(de)").show();
 // $('[lang="de"]').show(); // show just derean text (you can change it)
-$("#lang-switch").change(function () {
-  console.log("SWITCH");
-  console.log($(this).val());
-  // put onchange event when user select option from select
-  var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
-  switch (lang) {
-    case "en":
-      $(":lang(en)").show();
-      $(":lang(de)").hide();
-      //   $("[lang]").hide();
-      //   $('[lang="en"]').show();
-      break;
-    case "de":
-      $(":lang(en)").hide();
-      $(":lang(de)").show();
-      //   $("[lang]").hide();
-      //   $('[lang="de"]').show();
-      break;
-    default:
-      $(":lang(en)").hide();
-      $(":lang(de)").show();
-    //   $("[lang]").hide();
-    //   $('[lang="de"]').show();
+function tog() {
+  // Get the checkbox
+  var checkBox = document.getElementById("temp");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true) {
+    // text.style.display = "block";
+    $(":lang(en)").show();
+    $(":lang(de)").hide();
+  } else {
+    $(":lang(en)").hide();
+    $(":lang(de)").show();
   }
-});
+}
+
+// $("#lang-switch").change(function () {
+//   console.log("SWITCH");
+//   console.log($(this).val());
+
+//   var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
+
+//   console.log("HMM", lang);
+//   switch (lang) {
+//     case "en":
+//       $(":lang(en)").show();
+//       $(":lang(de)").hide();
+
+//       break;
+//     case "de":
+//       $(":lang(en)").hide();
+//       $(":lang(de)").show();
+
+//       break;
+//     default:
+//       $(":lang(en)").hide();
+//       $(":lang(de)").show();
+
+//   }
+// });
---
+++ section-transitions.js
@@ -0,0 +1,18 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("act");
+    } else {
+      reveals[i].classList.remove("act");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
